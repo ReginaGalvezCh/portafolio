b800485c91449ce1ecf10055074a17da0f3ebdc8
Regina Galvez Chavez <reginagalvezchavez@MBP-de-Regina.iptv.microsoft.com>
2017-03-27 17:02:30 -0300

    ajustes

--- js/main.js
+++ js/main.js
@@ -5,15 +5,15 @@ $(".hover").mouseleave(
 );
 
 $(document).ready(function(){
-  // Add smooth scrolling to all links
+
   $("a").on('click', function(event) {
 
-    // Make sure this.hash has a value before overriding default behavior
+   
     if (this.hash !== "") {
-      // Prevent default anchor click behavior
+     
       event.preventDefault();
 
-      // Store hash
+     
       var hash = this.hash;
 
       
@@ -34,15 +34,15 @@ $(window).on("load",function() {
   $(window).scroll(function() {
     var windowBottom = $(this).scrollTop() + $(this).innerHeight();
     $(".fade").each(function() {
-      /* Check the location of each desired element */
+      
       var objectBottom = $(this).offset().top + $(this).outerHeight();
       
-      /* If the element is completely within bounds of the window, fade it in */
-      if (objectBottom < windowBottom) { //object comes into view (scrolling down)
+      
+      if (objectBottom < windowBottom) { 
         if ($(this).css("opacity")==0) {$(this).fadeTo(500,1);}
-      } else { //object goes out of view (scrolling up)
+      } else { 
         if ($(this).css("opacity")==1) {$(this).fadeTo(500,0);}
       }
     });
-  }).scroll(); //invoke scroll-handler on page-load
+  }).scroll(); 
 });
